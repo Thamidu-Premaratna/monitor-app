8a6abce953abde97041be1056a9a860fdf3ed75c
SavitarA2466 <sangeethgeeth74@gmail.com>
2024-06-16 09:36:25 +0530

    Sensor., SensorData Done

---
+++ backend/src/main/java/com/monitor/api/entity/SensorData.java
@@ -0,0 +1,26 @@
+package com.monitor.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sensor_data")
+public class SensorData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // Unique identifier for the sensor data
+    @ManyToOne
+    @JoinColumn(name = "sensor_id")
+    @JsonManagedReference
+    private Sensor sensor; // Will be sensor_id in the SensorDataDTO
+    private LocalDateTime date;
+    private String dataValue;
+}
